df9d2ceedd1b9e70327cb87174f0b74eaea40698
Vagrant55 <121515731+Vagrant55@users.noreply.github.com>
2025-09-24 20:03:42 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -13,10 +13,10 @@ const SPREADSHEET_ID = "1utCG8rmf449THR5g6SHvSK4pp6-nj7UEgSgP4H1_isc";
 const SERVICE_ACCOUNT = {
   "type": "service_account",
   "project_id": "kaf-471314",
-  "private_key_id": "5a6f71bc00b6b29c61642fc35de7d505d34e2dcf",
+  "private_key_id": "6650093997955a7f8e1341a4a8f5482ed84ca354",
   "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  "client_email": "id-22-bot@kaf-471314.iam.gserviceaccount.com",
-  "client_id": "118228097079633655651",
+  "client_email": "bot22kaf@kaf-471314.iam.gserviceaccount.com.",
+  "client_id": "115744004771507568867",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
