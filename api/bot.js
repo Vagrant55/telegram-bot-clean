34493cad742e8d44581188b6d77984466dc56ffc
Vagrant55 <121515731+Vagrant55@users.noreply.github.com>
2025-09-24 22:21:01 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -20,7 +20,7 @@ const SERVICE_ACCOUNT = {
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/id-22-bot%40kaf-471314.iam.gserviceaccount.com",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bot22kaf%40kaf-471314.iam.gserviceaccount.com",
   "universe_domain": "googleapis.com"
 };
 
